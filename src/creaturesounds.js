f04d312142c2877ebff0bc23bbd726f1222f8376
Oli Lan <mail@olilan.co.uk>
2024-08-11 20:56:13 +0100

    Rename soundbynameortrait to creaturesounds

---
+++ src/creaturesounds.js
@@ -0,0 +1,99 @@
+import soundsDatabase from "../databases/sounds_db.json" assert { type: "json" };
+
+const HIT_SOUND_VOLUME = 0.5;
+
+export function creatureSoundOnDamage(actor) {
+    const soundType = checkIfDamageKills(actor);
+
+    //check for name match first
+    let returnedSounds = findSoundByCreatureName(actor.name, soundType)
+    console.log("value of returnedSounds after findSoundByCreatureName(): " + returnedSounds)
+    if (!returnedSounds) {
+        //fallback to trait match
+        const rollOptions = actor.flags.pf2e.rollOptions.all;
+        returnedSounds = findSoundByTraits(extractTraits(rollOptions), soundType);
+    }
+
+    if (returnedSounds) {
+        // Found something!
+        playRandomSound(returnedSounds);
+    } else {
+        // Didn't find anything
+        console.log("No Sounds found.")
+    }
+}
+
+function checkIfDamageKills(actor) {
+    if (actor.system.attributes.hp.value === 0) {
+        return "death"
+    }
+    return "hit"
+}
+
+function findSoundByCreatureName(creatureName, soundType) {
+    for (const [key, value] of Object.entries(soundsDatabase)) {
+        if (value.creatures?.includes(creatureName)) {
+            console.log("Exact Match found for " + creatureName);
+            return getSoundsOfType(value, soundType);
+        }
+    }
+    console.log("Could not find in db: " + creatureName)
+    return null;
+}
+
+function findSoundByTraits(traits, soundType) {
+    let bestMatch = null;
+    let maxMatchingTraits = 0;
+    console.log("Traits found for damaged creature are: " + traits);
+    for (const [key, value] of Object.entries(soundsDatabase)) {
+        const matchingTraits = value.traits.filter(trait => traits.includes(trait)).length;
+        if (matchingTraits > maxMatchingTraits) {
+            bestMatch = value;
+            maxMatchingTraits = matchingTraits;
+        }
+    }
+    if (bestMatch) {
+        return getSoundsOfType(bestMatch, soundType);
+    }
+    return null;
+}
+
+function getSoundsOfType(dbValue, soundType) {
+    switch (soundType) {
+        case 'hit':
+            return dbValue.hit_sounds;
+        case 'death':
+            if (dbValue.death_sounds.length != 0) {
+                return dbValue.death_sounds;
+            }
+            console.log("No death sounds found, so using hit sound as fallback");
+            return dbValue.hit_sounds;
+        default:
+            console.log("No sounds found for soundType=" + soundType);
+    }
+}
+
+function extractTraits(obj) {
+    const traits = [];
+    for (const key in obj) {
+        if (key.startsWith("self:trait:")) {
+            const trait = key.replace("self:trait:", "");
+            traits.push(trait);
+        }
+    }
+    return traits;
+}
+
+function playRandomSound(sounds) {
+    playSound(sounds[Math.floor(Math.random() * sounds.length)]);
+}
+
+function playSound(sound) {
+    console.log("sound to play:" + sound);
+    foundry.audio.AudioHelper.play({
+        src: sound,
+        volume: HIT_SOUND_VOLUME,
+        autoplay: true,
+        loop: false
+    }, true);
+}
